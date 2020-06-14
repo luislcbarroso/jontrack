67bc670b409b40a3bbfc0a68e4101c12cc80f7e2
luislcbaroso <luis.barroso@ifood.com.br>
2020-06-14 02:47:57 -0300

    get nearby places

---
+++ src/Clients/maps.js
@@ -0,0 +1,13 @@
+import { Client, Status } from '@googlemaps/google-maps-services-js'
+
+const client = new Client({})
+
+export const getPlacesNearby = (lat, lng, type, radius = 2000) =>
+  client.placesNearby({
+    params: {
+      key: process.env.GOOGLE_PLACES_API,
+      location: { lat, lng },
+      radius,
+      type,
+    },
+  })
--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import 'dotenv/config'
 
+import { concat } from 'ramda'
 import cors from 'cors'
 import express from 'express'
+import { getPlacesNearby } from './Clients/maps'
 
 const app = express()
 app.use(cors())
 app.disable('x-powered-by')
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  try {
+    const {lat, lng, radius} = req.query
+    const getGasStations = getPlacesNearby(lat, lng, 'gas_station', radius).catch((e) => console.log(e))
+    const getHospitals = getPlacesNearby(lat, lng, 'hospital', radius).catch((e) =>
+    console.log(e)
+    )
+    
+    Promise.all([getGasStations, getHospitals]).then((responses) => res.send(responses.reduce((acc, { data }) => concat(acc, data.results), [])))
+    .catch(() => res.send(Error('internal server error')))
+  }
+  catch {
+    res.send(Error('internal server error'))
+  }
 })
+
+
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on port: ${process.env.PORT}`)
 })
